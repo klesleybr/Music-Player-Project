57232ee6b9fc37d4ad56a92f1ecc76fa5371d8d5
klesley <klesley.pfeitosa@gmail.com>
2025-04-18 04:41:39 -0300

    test: início do desenvolvimento do player

---
+++ app/player.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+export default function player() {
+    return(
+
+        <View>
+        
+            <Text>Hello, world!</Text>
+        
+        </View>
+
+    );
+}
